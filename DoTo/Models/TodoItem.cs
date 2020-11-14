33a56f9c120a3995d4e5800c6d38242cbae31be8
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2020-11-13 20:08:40 -0500

    add model and repository ToDo the item

---
+++ DoTo/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using System;
+
+namespace DoTo.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+        public DateTime Due { get; set; }
+    }
+}
---
+++ DoTo/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DoToo.Models;
+
+namespace DoTo.Repositories
+{
+    public interface ITodoItemRepository
+    {
+        event EventHandler<TodoItem> OnItemAdded;
+        event EventHandler<TodoItem> OnItemUpdated;
+
+        Task<List<TodoItem>> GetItems();
+        Task AddItem(TodoItem item);
+        Task UpdateItem(TodoItem item);
+        Task AddOrUpdate(TodoItem item);
+    }
+}
